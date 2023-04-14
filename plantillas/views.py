24702d637338cac14e95b9c9783a872879ed8671
Enm4nuel <katariinaop@gmail.com>
2023-04-14 16:40:25 -0400

    refactor plantillas app

--- plantillas/views.py
+++ plantillas/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render, redirect
 import os
-import pathlib
 import pandas as pd
 import numpy as np
 
 from openpyxl import Workbook
 from openpyxl.styles import Font, Color
 
-from .models import Plantilla
+from .models import Template, TemplateLog, TemplateMonthlyCfgLog, TemplateMonthlyCfg
 
 import pyodbc
 
@@ -17,15 +16,12 @@ username = 'Data_Editor'
 password = 'jr03124300'
 # ENCRYPT defaults to yes starting in ODBC Driver 18. It's good to always specify ENCRYPT=yes on the client side to avoid MITM attacks.
 
-def cargar_datos(edificio, moneda):
+def loadData(building, coin):
 
 	cnxn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=no;UID='+username+';PWD='+ password)
-	print(edificio)
 	cursor = cnxn.cursor()
-	cursor.execute("SELECT * FROM Vpfacturacion WHERE BLDGID = '%s'" % edificio)
+	cursor.execute("SELECT * FROM Vpfacturacion WHERE BLDGID = '%s'" % building)
 	row = cursor.fetchall()
-
-	print("funcion cargar datos bien")
 
 	leasid = []
 	bldgid = []
@@ -43,119 +39,29 @@ def cargar_datos(edificio, moneda):
 		suitid.append(row[i][2])
 		occpname.append(row[i][3])
 
-	p = Plantilla.objects.all()
-	for i in p:
-		if i.bldgid == edificio and i.currcode == moneda:
-			descripciones.append(i.descrptn)
+	t = Template.objects.all()
+	print(t)
+	for i in t:
+		for k, v in i.fields.items():
+			if i.bldgid == building and i.currcode == coin:
+				descripciones.append(v)
 
-	generar_plantilla(moneda, edificio, descripciones, leasid, bldgid, suitid, occpname)
+	generar_plantilla(coin, building, descripciones, leasid, bldgid, suitid, occpname)
 	cursor.close()
 
 	return 0
 
-"""
-def cargar_datos(edificio, moneda):
-
-	cnxn = pyodbc.connect('DRIVER={ODBC Driver 18 for SQL Server};SERVER='+server+';DATABASE='+database+';ENCRYPT=no;UID='+username+';PWD='+ password)
-
-	if edificio == "T-CAM":
-
-		cursor = cnxn.cursor()
-		cursor.execute("SELECT * FROM Vpfacturacion WHERE BLDGID='T-CAM' ")
-		row = cursor.fetchall()
-
-		leasid = []
-		bldgid = []
-		suitid = []
-		occpname = []
-		descripciones = []
-
-		for i in range(len(row)):
-			leasid.append(row[i][0])
-			bldgid.append(row[i][1])
-			suitid.append(row[i][2])
-			occpname.append(row[i][3])
-
-		p = Plantilla.objects.all()
-		for i in p:
-			if i.bldgid == 'T-CAM':
-				descripciones.append(i.descrptn)
-
-		print(descripciones)
-		generar_plantilla(moneda, edificio, descripciones, leasid, bldgid, suitid, occpname)
-		cursor.close()
-
-	elif edificio == "BM-SDC":
-
-		cursor = cnxn.cursor()
-		cursor.execute("SELECT * FROM Vpfacturacion WHERE BLDGID='BM-SDC'")
-		row = cursor.fetchall()
-
-		leasid = []
-		bldgid = []
-		suitid = []
-		occpname = []
-		descripciones = []
-
-		for i in range(len(row)):
-			leasid.append(row[i][0])
-			bldgid.append(row[i][1])
-			suitid.append(row[i][2])
-			occpname.append(row[i][3])
-
-		p = Plantilla.objects.all()
-		for i in p:
-			if i.bldgid == 'BM-SDC':
-				descripciones.append(i.descrptn)
-				print(i.descrptn)
-
-		print(descripciones)
-		generar_plantilla(moneda, edificio, descripciones, leasid, bldgid, suitid, occpname)
-		cursor.close()
-
-	elif edificio == "BM-PCC":
-		
-		cursor = cnxn.cursor()
-		cursor.execute("SELECT * FROM Vpfacturacion WHERE BLDGID='BM-PCC'")
-		row = cursor.fetchall()
-
-		leasid = []
-		bldgid = []
-		suitid = []
-		occpname = []
-		descripciones = []
-
-		for i in range(len(row)):
-			leasid.append(row[i][0])
-			bldgid.append(row[i][1])
-			suitid.append(row[i][2])
-			occpname.append(row[i][3])
-
-		p = Plantilla.objects.all()
-		for i in p:
-			if i.bldgid == 'BM-PCC':
-				descripciones.append(i.descrptn)
-				print(i.descrptn)
-
-		print(descripciones)
-		generar_plantilla(moneda, edificio, descripciones, leasid, bldgid, suitid, occpname)
-		cursor.close()
-	
-	else:
-		print("No se selecciono ningun edificio")
-
-	return 0
-"""
-
-def generar_plantilla(moneda, edificio, index = [], leasid = [], bldgid = [], suitid = [], occpname = []):
+def generar_plantilla(coin, building, index = [], leasid = [], bldgid = [], suitid = [], occpname = []):
 	d = {'LEASID': leasid, 'BLDGID': bldgid, 'SUITID': suitid, 'OCCPNAME': occpname}
 	for i in range(len(index)):
 		d[str(index[i])] = ""
 	df = pd.DataFrame(data=d)
-	df.to_excel("Plantilla" + edificio + "_" + moneda + ".xlsx")
+	df.to_excel("Plantilla" + building + "_" + coin + ".xlsx")
+
+	TemplateLog.objects.create(bldgid=building, batch=3435)
 
 
-def generar_plantilla2(moneda, edificio, index = [], leasid = [], bldgid = [], suitid = [], occpname = []):
+def prueba_generar_plantilla(moneda, edificio, index = [], leasid = [], bldgid = [], suitid = [], occpname = []):
 
 	wb = Workbook()
 	ws = wb.active
@@ -192,7 +98,7 @@ def generar_plantilla2(moneda, edificio, index = [], leasid = [], bldgid = [], s
 	wb.save("Plantilla" + edificio + "_" + moneda + ".xlsx")
 
 
-
+"""
 def modificar_plantillas(request):
 
 	if request.method == 'POST':
@@ -234,4 +140,4 @@ def modificar_plantillas_editar(request, id):
 	if request.method == 'POST':
 		pass
 
-	return redirect('/plantillas/modificar_plantillas')
+	return redirect('/plantillas/modificar_plantillas')"""
--- to_mri/models.py
+++ to_mri/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from plantillas.models import Plantilla
 
 # Create your models here.
+
+class ToMri(models.Model):
+	BLDGID = models.ForeignKey(Plantilla, on_delete=models.CASCADE)
+	LEASID = models.CharField(max_length=10)
+	DESCRPTN = models.CharField(max_length=30)
+	VALUE = models.DecimalField(max_digits=9, decimal_places=2)
+
+	def __str__(self):
+		template = '{0.BLDGID} {0.LEASID} {0.DESCRPTN} {0.VALUE}'
+		return template.format(self)
