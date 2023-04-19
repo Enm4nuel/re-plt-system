af9143f65a1fc81c316d230c025eed3aa159a704
Enm4nuel <katariinaop@gmail.com>
2023-04-19 18:07:31 -0400

    guardado

--- home/static/js/descargar_plantilla.js
+++ home/static/js/descargar_plantilla.js
@@ -2,6 +2,7 @@ function habilitar()
 {
 	edificio = document.getElementById('edificio').value;
 	moneda = document.getElementById('moneda').value;
+	batch = document.getElementById('batch').value;
 	val = 0;
 
 	if (edificio != "Selecciona el edificio...")
@@ -12,16 +13,33 @@ function habilitar()
 	else
 	{
 		document.getElementById('moneda').disabled = true;
-		document.getElementById('moneda').value = "Selecciona la moneda...";	
+		document.getElementById('moneda').value = "Selecciona la moneda...";
+		document.getElementById('batch').value = "";
+		document.getElementById('batch').disabled = true;
+		
 	}
 
 	if (moneda != "Selecciona la moneda...")
 	{
 		val++;
+		document.getElementById('batch').disabled = false;
+	}
+	else
+	{
+		document.getElementById('batch').disabled = true;
+		document.getElementById('batch').value = "";
+	}
+
+	if (batch != null)
+	{
+		if (batch.length() == 8)
+		{
+			val++;
+		}
 	}
 	
 
-	if (val == 2)
+	if (val == 3)
 	{
 		document.getElementById("btn_gen_plt").disabled = false;
 	}
